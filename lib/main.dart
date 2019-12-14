70ea103b1359240a4731d7500c3feb18cda06b1c
AbdullahChauhan <acse.uok@gmail.com>
2019-12-14 13:00:52 +0500

    done with final touch to the app...

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.redAccent),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('WordPair Generator'),
-        ),
-        body: Center(
-          child: Text('${wordPair.asPascalCase}', style: TextStyle(
-            fontSize: 32,
-            letterSpacing: 1.5
-          ),),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: RandomWords(),
     );
   }
 }
